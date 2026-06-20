e598e99141d8aa0db4f48963c730d92b71f1b5ff
Charanjeet <singhcharanjeet603@gmail.com>
2026-06-20 13:04:15 +0200

    feat: implement tabs layout with navigation and sign-out functionality

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
 import { useAuth } from "@clerk/expo"
 import { Pressable, Text, View } from "react-native"
+
 export default function HomeScreen() {
    const { signOut } = useAuth()
 
    return (
-      <View className="flex-1 items-center justify-center bg-primary dark:bg-secondary">
+      <View className="flex-1 items-center justify-center">
          <Text>Home Screen</Text>
          <Pressable onPress={() => signOut()}>
             <Text>Sign out</Text>
